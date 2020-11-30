96dbf926de344edbaa84d81c61ed9d5908d2bb34
Xavier Eugenio Asuncion <ginoasuncion@gmail.com>
2020-11-30 15:44:46 +0800

    update image colorization app

---
+++ colorize-image.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import numpy as np
+import argparse
+import cv2
+from PIL import Image
+
+st.title("Colorize your photo")
+st.subheader('Use AI to automatically colorize your black and white photos.')
+
+# load our serialized black and white colorizer model and cluster
+# center points from disk
+prototxt = '/Users/ginoasuncion/Projects/image-colorization/model/colorization_deploy_v2.prototxt'
+model = '/Users/ginoasuncion/Projects/image-colorization/model/colorization_release_v2.caffemodel'
+points = '/Users/ginoasuncion/Projects/image-colorization/model/pts_in_hull.npy'
+
+uploaded_file = st.file_uploader("Choose an image..")
+
+# if st.button('Colorize'):
+if uploaded_file is not None:
+    net = cv2.dnn.readNetFromCaffe(prototxt, model)
+    pts = np.load(points)
+
+    # add the cluster centers as 1x1 convolutions to the model
+    class8 = net.getLayerId("class8_ab")
+    conv8 = net.getLayerId("conv8_313_rh")
+    pts = pts.transpose().reshape(2, 313, 1, 1)
+    net.getLayer(class8).blobs = [pts.astype("float32")]
+    net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+    # load the input image from disk, scale the pixel intensities to the
+    # range [0, 1], and then convert the image from the BGR to Lab color
+    # space
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, 1)
+    scaled = image.astype("float32") / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+
+    # resize the Lab image to 224x224 (the dimensions the colorization
+    # network accepts), split channels, extract the 'L' channel, and then
+    # perform mean centering
+    resized = cv2.resize(lab, (224, 224))
+    L = cv2.split(resized)[0]
+    L -= 50
+
+    # pass the L channel through the network which will *predict* the 'a'
+    # and 'b' channel values
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+
+    # resize the predicted 'ab' volume to the same dimensions as our
+    # input image
+    ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
+
+    # grab the 'L' channel from the *original* input image (not the
+    # resized one) and concatenate the original 'L' channel with the
+    # predicted 'ab' channels
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+
+    # convert the output image from the Lab color space to RGB, then
+    # clip any values that fall outside the range [0, 1]
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
+
+    # the current colorized image is represented as a floating point
+    # data type in the range [0, 1] -- let's convert to an unsigned
+    # 8-bit integer representation in the range [0, 255]
+    colorized = (255 * colorized).astype("uint8")
+
+    st.header('Original image')
+    st.image(image, channels="BGR")
+
+    st.header('Colorized image')
+    st.image(colorized, channels="BGR")
